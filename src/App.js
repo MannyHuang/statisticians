d5f262689d592b4e4a08e8957c57309dc1449955
MannyHuang <416403086@qq.com>
2020-03-04 20:21:07 +0800

    added header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/HomPage';
 import './App.css';
 import PageOne from './pages/pageone/PageOne';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/pageone' component={PageOne} />
