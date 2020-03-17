1727a37bf824724606f424ac58520c6138969d7a
MannyHuang <416408036@qq.com>
2020-03-18 01:05:07 +0800

    Updates

---
+++ precache-manifest.a7533f2d3a69ebe34481aaacb8ace7f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78fcc1fa9f0bf863c3c23014dfbca6a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0320427d1bc30b3e279d",
+    "url": "/static/css/main.025803db.chunk.css"
+  },
+  {
+    "revision": "c1f3a6f219b92adadbce",
+    "url": "/static/js/2.e7f42f05.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/static/js/2.e7f42f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0320427d1bc30b3e279d",
+    "url": "/static/js/main.9b68e650.chunk.js"
+  },
+  {
+    "revision": "95eb7ef1592627fdf77a",
+    "url": "/static/js/runtime-main.42ff529b.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/Logo.ed28accb.svg"
+  }
+]);
